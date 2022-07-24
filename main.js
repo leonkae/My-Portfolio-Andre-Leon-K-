b291b2e5ed81841fbb8179ddcdabab16d68c6370
leonkae <leon29kae@gmail.com>
2022-07-24 21:40:21 +0300

    add: images, svg modify: index, main js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let number = document.getElementById("number");
 let counter = 0;
 setInterval(() =>{
-    if(counter ==90){
+    if(counter ==83){
         clearInterval();
     }else{
         counter += 1;
         number.innerHTML = counter + "%";
     }
 
-}, 45);
+}, 48);
