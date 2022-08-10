42c0dec176ad1e0a02e3b9df2ba610429230c9fb
leonkae <leon29kae@gmail.com>
2022-08-10 23:24:20 +0300

    add:updated toggler

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if (this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
-        body.style.color = 'black'
+        body.style.color = 'black';
         body.style.transition ='2.3s';
     }else{
         body.style.background = 'black';
