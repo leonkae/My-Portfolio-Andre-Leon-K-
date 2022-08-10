ca346535ea196d848093ef3246f44cd5c9b26762
leonkae <leon29kae@gmail.com>
2022-08-10 22:50:12 +0300

    add:theme selector functions

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ setInterval(() =>{
     }
 
 }, 48);
+
+// function myFunction(){
+//     var element = document.body;
+//     element.classList.toggle("dark-mode")
+// }
+
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if (this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black'
+        body.style.transition ='2.3s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transtion = '2.3s'
+    }
+
+})
