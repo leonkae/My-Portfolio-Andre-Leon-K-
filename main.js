c000703df9da88d30112a346a5628b06fde382a6
leonkae <leon29kae@gmail.com>
2022-08-11 13:09:16 +0300

    update: theme icon

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const body = document.querySelector('body');
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
-    if (this.classList.toggle('bi-brightness-high-fill')){
+    if (this.classList.toggle('bi-sun-high-fill')){
         body.style.background = 'white';
         body.style.color = 'black';
         body.style.transition ='2.3s';
